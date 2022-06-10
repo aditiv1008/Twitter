108c3c0a935de190636e69098aedc937df2c0ed4
Aditi Vishwanath <aditiv1008@fb.com>
2022-06-10 02:13:52 -0700

    optional features

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/DetailedView.java
@@ -0,0 +1,16 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailedView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_view);
+
+
+    }
+}
